ba45831926df8cb301eec7f4c3069ca6aadeb7c5
tyhbergman <112422622+tyhbergman@users.noreply.github.com>
2023-03-24 11:26:19 -0600

    Updated to 1.ps3
    Added feature:
    
    -I/O Redirection

--- shell.cpp
+++ shell.cpp
@@ -1,7 +1,7 @@
 /* Ty Bergman
-03/20/2023
+03/24/2023
 CS 461
-ps2 - Simple Shell */
+ps3 - Simple Shell with I/O Redirection */
 
 #include <iostream>
 #include <string>
@@ -10,27 +10,42 @@ ps2 - Simple Shell */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h> // Include fcntl.h for open() function
+
+// Struct to more easily handle command strings
+struct ParsedInput {
+    std::vector<std::string> tokens;
+    std::string input_file;
+    std::string output_file;
+};
 
 // Function to parse user input into command and arguments
-std::vector<std::string> parse_input(const std::string &input) {
+ParsedInput parse_input(const std::string &input) {
     // Create an input string stream from input
     std::istringstream iss(input);
+
     // String variable to hold individual tokens
     std::string token;
-    // Vector to store all tokens
-    std::vector<std::string> tokens;
 
-    // Extract tokens from input string stream and store them in the vector
+    // Struct to store all tokens
+    ParsedInput parsed_input;
+
+    // Parse the input and store tokens in the struct
     while (iss >> token) {
-        tokens.push_back(token);
+        if (token == "<") {
+            iss >> parsed_input.input_file;
+        } else if (token == ">") {
+            iss >> parsed_input.output_file;
+        } else {
+            parsed_input.tokens.push_back(token);
+        }
     }
 
-    // Return the vector of tokens
-    return tokens;
-}
+    return parsed_input;
+};
 
 // Function to execute a command with its arguments
-void execute_command(const std::vector<std::string> &tokens) {
+void execute_command(const ParsedInput &parsed_input) {
     // Fork a new process
     pid_t pid = fork();
 
@@ -40,29 +55,52 @@ void execute_command(const std::vector<std::string> &tokens) {
         return;
     }
 
-    // Child process
-    if (pid == 0) {
-        // Convert the vector of strings into a vector of char pointers
-        std::vector<char *> argv(tokens.size() + 1);
-
-        // Fill the argv vector with pointers to the tokens
-        for (size_t i = 0; i < tokens.size(); ++i) {
-            argv[i] = &tokens[i][0];
+    if (pid == 0) { // Child process
+        // Handle input redirection
+        if (!parsed_input.input_file.empty()) {
+            //Create file descriptor
+            int input_fd = open(parsed_input.input_file.c_str(), O_RDONLY);
+            if (input_fd < 0) {
+                perror("Error opening input file");
+                exit(EXIT_FAILURE);
+            }
+            //dup2 to create copy
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
         }
-        // Set the last element of argv to nullptr
-        argv[tokens.size()] = nullptr;
 
-        // Execute the command with its arguments
+        // Handle output redirection
+        if (!parsed_input.output_file.empty()) {
+            //Create file descriptor with appropriate tags to handle if file already exists or not, and with read/write permission
+            int output_fd = open(parsed_input.output_file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (output_fd < 0) {
+                perror("Error opening output file");
+                exit(EXIT_FAILURE);
+            }
+            //dup2 to create copy
+            dup2(output_fd, STDOUT_FILENO);
+            close(output_fd);
+        }
+
+        // Convert the tokens from std::string to char* for the execvp function
+        std::vector<char *> argv(parsed_input.tokens.size() + 1);
+        for (size_t i = 0; i < parsed_input.tokens.size(); ++i) {
+            argv[i] = const_cast<char *>(parsed_input.tokens[i].c_str());
+        }
+        //Set last element to null for null termination
+        argv[parsed_input.tokens.size()] = nullptr;
+
+        // Execute the command
         if (execvp(argv[0], &argv[0]) == -1) {
             std::cerr << "Error executing command" << std::endl;
         }
-        // Exit the child process if the command execution fails
         exit(EXIT_FAILURE);
     } else { // Parent process
         // Wait for the child process to finish
         waitpid(pid, nullptr, 0);
     }
 }
+
 
 //Main function
 int main() {
@@ -81,34 +119,31 @@ int main() {
             continue;
         }
 
-        // Parse the input into command and arguments
-        std::vector<std::string> tokens = parse_input(input);
+        // Parse the input into command and arguments using ParsedInput struct
+        ParsedInput parsed_input = parse_input(input);
 
-        // If the command is "exit", break the loop and exit the program
-        if (tokens[0] == "exit") {
+        if (parsed_input.tokens.empty()) {
+            continue;
+        }
+
+        if (parsed_input.tokens[0] == "exit") {
             break;
         }
 
         // Check if the command is 'cd' and handle it separately
-        if (tokens[0] == "cd") {
-            // Ensure that the command has the correct number of arguments
-            if (tokens.size() == 2) {
-                // Attempt to change the directory and check for errors
-                if (chdir(tokens[1].c_str()) != 0) {
-                    // Print an error message if the directory change failed
+        if (parsed_input.tokens[0] == "cd") {
+            if (parsed_input.tokens.size() == 2) {
+                if (chdir(parsed_input.tokens[1].c_str()) != 0) {
                     perror("Error changing directory");
                 }
             } else {
-                // Print a usage message if the command has the wrong number of arguments
                 std::cerr << "Usage: cd <directory>" << std::endl;
             }
         } else {
-            // If the command is not 'cd', execute the command with its arguments
-            execute_command(tokens);
+            execute_command(parsed_input);
         }
     }
 
-    // Exit the program
     return 0;
 }
 
